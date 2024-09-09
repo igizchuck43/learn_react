49a6d27e8547b53f81316f5cc3ea5f28087d7c64
Igiz <igizstarkle@gmail.com>
2024-09-09 23:53:20 +0300

    Loops in react

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,20 @@ const App = () => {
     const x = 10;
     const y = 20;
 
+    const names = ['Charles', 'John', 'Paul', 'George', 'Ringo'];
+
   return (
     <div className="">
         <div className='text-5xl'>App</div>
         <p>Hello = {name} </p>
         <p>The sum of {x} and {y} is <strong>{x + y}</strong></p>
+
+        <p>The Loop</p>
+        <ul>
+            {names.map((names, index)=>(
+                <li key={index}>{names}</li>
+            ))}
+        </ul>
     </div>
     
   )
